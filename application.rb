f67e8eb6d4480f2bab8fd95c6b855675a53ae8b1
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-24 17:33:44 +0000

    automatically backed up by learn

--- application.rb
+++ application.rb
@@ -2,10 +2,9 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, World. "
     resp.write "My name is Tahseen Fatima."
     resp.finish
   end
 
 end
-
